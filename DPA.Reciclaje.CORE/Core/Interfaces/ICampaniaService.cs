99bab50ebcbfcb253c96bddd0a1d6c2f980a6be1
romulohh <romulohh@gmail.com>
2025-11-08 07:56:15 -0500

    Añadir gestión de campañas y servicios relacionados
    Se han agregado servicios para manejar campañas en la aplicación, incluyendo el registro de `ICampaniaRepository` y `ICampaniaService`. Se ha creado el controlador `CampaniaController` para operaciones CRUD, junto con los DTOs `CampaniaDTO` y `CampaniaResponseDTO`. También se han definido las interfaces `ICampaniaRepository` e `ICampaniaService`, y se han implementado las clases `CampaniaService` y `CampaniaRepository` para la lógica de negocio y acceso a datos, respectivamente.

---
+++ DPA.Reciclaje.CORE/Core/Interfaces/ICampaniaService.cs
@@ -0,0 +1,11 @@
+﻿using DPA.Reciclaje.CORE.Core.DTOs;
+
+namespace DPA.Reciclaje.CORE.Core.Interfaces
+{
+    public interface ICampaniaService
+    {
+        Task<int> CreateAsync(CampaniaDTO dto);
+        Task<IEnumerable<CampaniaResponseDTO>> GetAllAsync();
+        Task<CampaniaResponseDTO?> GetByIdAsync(int id);
+    }
+}
